fb6e2e18953286a510f009a7b8345e1de2e3231d
jmarti <jmarti@theinit.com>
2017-09-23 17:14:29 +0200

    close and open states

--- test/unit/specs/Envelope.spec.js
+++ test/unit/specs/Envelope.spec.js
@@ -1,7 +1,7 @@
 import Envelope from '@/components/envelope/Envelope.vue'
 import { mount } from 'avoriaz'
 
-describe('Envelop.vue', () => {
+describe('Envelope.vue', () => {
   let wrapper
   let vm
 
@@ -10,7 +10,26 @@ describe('Envelop.vue', () => {
     vm = wrapper.vm
   })
   it('sets ups', () => {
-    expect(vm).to.be.defined
+    expect(vm.state).to.eql('closed')
   })
+  describe('state changes when click', () => {
+    it('changes from closed to open', () => {
+      vm.state = 'closed'
+      vm.triggerState()
 
+      expect(vm.state).to.eql('open')
+    })
+    it('changes from open to closed', () => {
+      vm.state = 'open'
+      vm.triggerState()
+
+      expect(vm.state).to.eql('closed')
+    })
+    it('changes from unknown to open', () => {
+      vm.state = 'totallyUnknownAndMessedUp'
+      vm.triggerState()
+
+      expect(vm.state).to.eql('open')
+    })
+  })
 })
