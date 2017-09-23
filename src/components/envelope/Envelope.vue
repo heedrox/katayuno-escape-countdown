079651612dbb79b6458b48382951d6f20768cc04
jmarti <jmarti@theinit.com>
2017-09-23 17:14:16 +0200

    change envelope closed

--- src/components/envelope/Envelope.vue
+++ src/components/envelope/Envelope.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="envelope">
-      <img src="./envelope-closed.png">
+    <div class="envelope" v-on:click="triggerState" >
+      <img v-if="state === 'closed'" src="./envelope-closed.png">
+      <img v-if="state === 'open'" src="./envelope-open.png">
       <div class="envelope-number-holder">{{number}}</div>
     </div>
 </template>
@@ -16,7 +17,7 @@
     position: relative;
     color:#000;
     font-size: 5vw;
-    top:-90px;
+    top:-80px;
   }
 
   @media screen and (min-width:700px) {
@@ -27,14 +28,24 @@
 
   @media screen and (max-width:700px) {
     .envelope .envelope-number-holder {
-      font-size: 10vw;
-      top: -14vw;
+      font-size: 8vw;
+      top: -11.5vw;
     }
   }
 </style>
 <script>
   export default {
     name: 'envelope',
-    props: [ 'number' ]
+    props: [ 'number' ],
+    data () {
+      return {
+        state: 'closed'
+      }
+    },
+    methods: {
+      triggerState () {
+        this.state = this.state === 'open' ? 'closed' : 'open'
+      }
+    }
   }
 </script>
