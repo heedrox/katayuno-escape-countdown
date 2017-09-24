e11c93d5a3bc63ba61972b8fbefe978b41cfc21b
jmarti <jmarti@theinit.com>
2017-09-24 12:20:54 +0200

    change input style for keypad2

--- src/views/Keypad2View.vue
+++ src/views/Keypad2View.vue
@@ -1,9 +1,23 @@
 <template>
-  <escape-keypad :text-done="textDone" :relative-keyboard-coords="[32,52,67,92]" skin="/static/keypad-skins/keypad2.png" class="main-keypad"></escape-keypad>
+  <escape-keypad :text-done="textDone" :relative-keyboard-coords="[32,52,67,92]" skin="/static/keypad-skins/keypad2.png" class="main-keypad2"></escape-keypad>
 </template>
 <style>
-  .main-keypad {
+  .main-keypad2 {
     width: 100%;
+  }
+  .main-keypad2 .under-door p {
+    padding-top: 10vh;
+    font-size: 6vw;
+  }
+  .main-keypad2 .keypad input {
+    position:relative;
+    top: 21.5vh;
+    background-color:#000;
+    color:#fff;
+    border: none;
+  }
+  .main-keypad2 .keypad img {
+    max-width:100%;
   }
 </style>
 <script>
