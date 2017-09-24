2df14d98a28ca439e8e54deb5b674d5834824aa4
jmarti <jmarti@theinit.com>
2017-09-24 15:30:08 +0200

    add keypad3 !

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import BriefingsView from '../views/BreefingsView.vue'
 import CountdownView from '../views/CountdownView.vue'
 import Keypad1View from '../views/Keypad1View.vue'
 import Keypad2View from '../views/Keypad2View.vue'
+import Keypad3View from '../views/Keypad3View.vue'
 
 Vue.use(Router)
 
@@ -30,5 +31,8 @@ export default new Router({
   }, {
     path: '/keypad2',
     component: Keypad2View
+  }, {
+    path: '/keypad3',
+    component: Keypad3View
   }]
 })
---
+++ src/views/Keypad3View.vue
@@ -0,0 +1,39 @@
+<template>
+  <escape-keypad :text-done="textDone" :relative-keyboard-coords="[17,18,65,73]" skin="/static/keypad-skins/keypad3.png"
+                 class="main-keypad3" combination-hash="$2a$10$5AHlWMVICV7OQlrbz/3OQegonQBWNLddgaX9gtdYLE4XysE1YSNlS"
+                 :number-of-digits="3"></escape-keypad>
+</template>
+<style>
+  .main-keypad3 {
+    width: 100%;
+  }
+  .main-keypad3 .under-door p {
+    padding-top: 10vh;
+    font-size: 6vw;
+  }
+  .main-keypad3 .keypad input {
+    position:relative;
+
+    background-color:#000;
+    color:#fff;
+    border: solid 1px #fff;
+  }
+  .main-keypad3 .keypad img {
+    max-width:100%;
+  }
+</style>
+<script>
+  import EscapeKeypad from '../components/Keypad.vue'
+
+  export default {
+    components: {
+      EscapeKeypad
+    },
+    data () {
+      return {
+        textDone: '<p>¡Enhorabuena!</p><p>¡Has terminado el Katayuno Escape Edition!</p><p>¡Y has evitado una catástrofe gracias a tus habilidades!</p><p><b>¡Felicidades!</b></p>'
+      }
+    }
+  }
+</script>
+
